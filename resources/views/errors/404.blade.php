b96b57fdc05021c3a217737c8b8bb0133d66e361
Julio Rahman <julio.rahman@gmail.com>
2021-02-21 18:09:43 +0700

    fix: fixing 404 error page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,21 +1,16 @@
-@extends('layouts.layout')
+@extends(Auth::guest() ? 'layouts.app' : 'layouts.layout')
 
-@section('title')
-404
-@endsection
+@section('title', '404')
 
 @section('content')
-<!-- Begin Page Content -->
-<div class="container-fluid">
-
-    <!-- 404 Error Text -->
-    <div class="text-center">
-        <div class="error mx-auto" data-text="404">404</div>
-        <p class="lead text-gray-800 mb-5">Halaman Tidak Ditemukan</p>
-        <p class="text-gray-500 mb-0">Sepertinya Anda menemukan kesalahan dalam matriks ...</p>
-        <a href="{{ route('dashboard') }}">&larr; Kembali ke Dasbor</a>
+<div class="@if(Auth::guest()) jr-centered @endif">
+    <div class="@if(Auth::guest()) bg-white @else container-fluid @endif p-5 ">
+        <div class="text-center">
+            <div class="error mx-auto" data-text="404">404</div>
+            <p class="lead text-gray-800 mb-5">Halaman Tidak Ditemukan</p>
+            <p class="text-gray-500 mb-0">Sepertinya Anda menemukan kesalahan dalam matriks ...</p>
+            @if(!Auth::guest()) <a href="{{ route('dashboard') }}">&larr; Kembali ke Dasbor</a> @endif
+        </div>
     </div>
-
 </div>
-<!-- /.container-fluid -->
 @endsection
